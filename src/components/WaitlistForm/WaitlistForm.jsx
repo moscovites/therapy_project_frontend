1d2e4fed6bd2bccf002d877dab1e943026d100c7
shaphat-codes <jehoshaphattatiglo99@gmail.com>
2024-01-10 10:39:20 +0000

    created a new build folder for testing

--- src/components/WaitlistForm/WaitlistForm.jsx
+++ src/components/WaitlistForm/WaitlistForm.jsx
@@ -16,7 +16,7 @@ const WaitlistForm = () => {
           <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
           </svg>
-          <span className="sr-only">Close modal</span>
+          <span className="sr-only">Close modall</span>
         </button>
       </div>
       {/* Modal body */}
