59c9549b2608379720ea993e05686723c98eaf0b
shaphat-codes <jehoshaphattatiglo99@gmail.com>
2024-04-06 15:14:05 -0700

    removed bug

--- src/Therapists/Dashboard/TherapistDashboard.jsx
+++ src/Therapists/Dashboard/TherapistDashboard.jsx
@@ -180,7 +180,7 @@ const TherapistDashboard = () => {
         <p class="mt-2 text-gray-500">Email: jehoshaphattatiglo99@gmail.com</p>
         
         <button class="w-25 mt-5 ml-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
-           More >>>
+           More
         </button>
         <button class="w-25 mt-5 ml-3 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500">
              Cancel
